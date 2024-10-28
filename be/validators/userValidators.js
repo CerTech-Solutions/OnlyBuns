9d32c43d3283e82eefbe9782171bb1c99a8ff867
nkusla <nikola.kuslakovic1616@gmail.com>
2024-10-28 11:52:12 +0100

    refac: Refactored user validator

--- be/validators/userValidators.js
+++ be/validators/userValidators.js
@@ -1,9 +1,10 @@
 const { body } = require('express-validator');
 
 exports.registerValidator = [
-	body('username').isLength({ min: 4, max: 10 }).withMessage('Username must be between 4 and 10 characters long'),
+	body('username').isLength({ min: 5, max: 15 }).withMessage('Username must be between 5 and 15 characters long'),
 	body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
 	body('email').isEmail().withMessage('Email must be valid'),
+	body('address').notEmpty().withMessage('Address must not be empty'),
 ];
 
 exports.loginValidator = [
